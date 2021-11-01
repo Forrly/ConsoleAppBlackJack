2d27cd76c727fab0b4c6047387331fa55ecc9cb4
Forly <vorrrl@mail.ru>
2021-11-01 18:49:36 +0300

    Adding tests for Card class

---
+++ ConsoleAppBlackJackTDDTesting/CardTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleAppBlackJackTDD;
+
+namespace ConsoleAppBlackJackTDDTesting
+{
+    public class CardTest
+    {
+        private Card _card;
+        [TestMethod]
+        [TestInitialize()]
+        public void Check_ConstructorCard()
+        {
+            _card = new Card(6);
+        }
+        [TestMethod]
+        public void CheckCard_Value()
+        {
+            Assert.AreEqual(6, _card.Value);
+        }
+        [TestMethod]
+        public void CheckCard_ToString()
+        {
+            Assert.AreEqual("6", _card.ToString());
+        }
+    }
+}
