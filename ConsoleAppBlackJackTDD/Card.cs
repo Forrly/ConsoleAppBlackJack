4200b8cd65ff359d3e79b8bf513ffa3c0c5dafd8
Forly <vorrrl@mail.ru>
2021-11-02 13:08:14 +0300

    Added empty methods for class Card

--- ConsoleAppBlackJackTDD/Card.cs
+++ ConsoleAppBlackJackTDD/Card.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace ConsoleAppBlackJackTDD
 {
-    public class Card
-    {
+    public class Card {
+        public int Value { get; }
+
+        public Card(int value){
+        }
+
+        public override string ToString(){
+        }
     }
 }
