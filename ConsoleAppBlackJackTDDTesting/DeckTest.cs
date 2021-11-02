574da0baffeff4a42d3c703855d67394cdde1ef6
Forly <vorrrl@mail.ru>
2021-11-02 12:06:02 +0300

    Adding tets for class Deck

---
+++ ConsoleAppBlackJackTDDTesting/DeckTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleAppBlackJackTDD;
+
+namespace ConsoleAppBlackJackTDDTesting
+{
+    [TestClass]
+    public class DeckTest
+    {
+        private Deck _deck;
+        [TestMethod]
+        [TestInitialize()]
+        public void Check_ConstructorDeck()
+        {
+            _deck = new Deck();
+        }
+        [TestMethod]
+        public void Check_ValueInTheRange_1_11_()
+        {
+            Card _card = _deck.GetCard();
+            Assert.AreEqual(1, objectsAreInRange(1, 11, _card.Value));
+        }
+
+        int objectsAreInRange(int actual, int expected, int delta)
+        {
+            if (delta > actual && delta < expected)
+                return 1;
+            else
+                return 0;
+        }
+        
+    }
+}
