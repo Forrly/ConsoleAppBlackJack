b2ce3cebda9111c0ddf2982a65588214dc940b54
Forly <vorrrl@mail.ru>
2021-11-02 13:11:46 +0300

    Added empty methods for Player class

--- ConsoleAppBlackJackTDD/Player.cs
+++ ConsoleAppBlackJackTDD/Player.cs
@@ -6,5 +6,21 @@ namespace ConsoleAppBlackJackTDD
 {
     public class Player
     {
+        public Hand hand;
+
+        public Player() {
+
+        }
+
+        public Hand Deal(Deck deck){
+
+        }
+
+        public void DealCards(int num, Deck deck){
+
+        }
+
+        public override string ToString(){
+        }
     }
 }
