ffd135cdad49ec6ecd7a31be6454853f24a18dc0
Forly <vorrrl@mail.ru>
2021-11-02 13:17:10 +0300

    Writing methods for Hand class

--- ConsoleAppBlackJackTDD/Hand.cs
+++ ConsoleAppBlackJackTDD/Hand.cs
@@ -8,18 +8,32 @@ namespace ConsoleAppBlackJackTDD
     {
         public ArrayList cards;
         public Hand() {
+            cards = new ArrayList();
         }
 
         public void AddCard(Card card){
+            cards.Add(card);
         }
 
         public bool IsBusted(){
+            return this.Total() > 21;
         }
 
         public int Total(){
+            int total = 0;
+            foreach(Card card in cards){
+                total += card.Value;
+            }
+            return total;
         }
         public override string ToString() {
-
+            string s = "Hand: ";
+            foreach(Card card in cards){
+                s += card + " ";
+            }
+            s += "\nTotal: ";
+            s += Total();
+            return s;
         }
     }
 }
